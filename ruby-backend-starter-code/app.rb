ced35d27e62e4993700f85208fb01174ddeb7b67
presto2116 <preston.straight@gmail.com>
2015-05-25 21:54:21 -0400

    added styling and javascript

--- ruby-backend-starter-code/app.rb
+++ ruby-backend-starter-code/app.rb
@@ -1,25 +1,28 @@
 require 'active_record'
 require 'sinatra'
+require 'json'
 
 
+  get '/' do
+    File.read('index.html')
+    erb :search
+  end
 
-get '/' do
-  File.read('index.html')
-end
+  get '/favorites' do
+    response.header['Content-Type'] = 'application/json'
+    File.read('data.json')
+  end
 
+  
+  post '/favorites' do
+    file = JSON.parse(File.read('data.json'))
+    unless params[:name] && params[:oid]
+      return 'Invalid Request'
+    movie = { name: params[:name], oid: params[:oid] }
+    file << movie
+    File.write('data.json',JSON.pretty_generate(file))
+    movie.to_json
+    redirect '/'
+  end
 
-get '/favorites' do
-  response.header['Content-Type'] = 'application/json'
-  File.read('data.json')
-end
-
-get '/favorites' do
-  file = JSON.parse(File.read('data.json'))
-  unless params[:name] && params[:oid]
-    return 'Invalid Request'
-  movie = { name: params[:name], oid: params[:oid] }
-  file << movie
-  File.write('data.json',JSON.pretty_generate(file))
-  movie.to_json
-end
 end
--- ruby-backend-starter-code/public/js/app.js
+++ ruby-backend-starter-code/public/js/app.js
@@ -1,3 +1,5 @@
+document.addEventListener("DOMContentLoaded", function(event) { 
+
 (function(){
 
   document.querySelector('form').addEventListener('submit', function(event){
@@ -5,52 +7,37 @@
     document.querySelector(".search").innerHTML= "";
     document.querySelector(".movie").innerHTML= "";
     var input = document.querySelector('input').value;
-    var xhr = new XMLHttpRequest();
-    xhr.open('get', 'http://omdbapi.com/?s=' + encodeURIComponent(input), true);
-    xhr.addEventListener('load', function(response){
+    var movieSearch = new XMLHttpRequest();
+    movieSearch.open('get', 'http://omdbapi.com/?s=' + encodeURIComponent(input), true);
+    movieSearch.addEventListener('load', function(response){
       var res =  JSON.parse(this.response).Search;
       for(var i = 0; i < res.length; i++){
         var node = document.createElement('li');
         node.innerText = res[i].Title;
         document.querySelector('.search').appendChild(node);
-        
-
-                            node.addEventListener('click', function(){
-                              document.querySelector(".movie").innerHTML = "";
-                              var aj = new XMLHttpRequest();
-                              aj.open('get', 'http://omdbapi.com/?t=' + this.innerHTML, true);
-                              aj.addEventListener('load', function(response){
-                                  var respon = JSON.parse(this.response)
-                            
-                                var titleNode = document.createElement('li');
-                                var yearNode = document.createElement('li');
-                                var ratedNode = document.createElement('li');
-                                var genreNode = document.createElement('li');
-                                var plotNode = document.createElement('li');
-                                titleNode.innerText = respon.Title;
-                                yearNode.innerText += respon.Year;
-                                ratedNode.innerText += respon.Rated;
-                                genreNode.innerText += respon.Genre;
-                                plotNode.innerText += respon.Plot;
-                                document.querySelector('.movie').appendChild(titleNode);
-                                document.querySelector('.movie').appendChild(yearNode);
-                                document.querySelector('.movie').appendChild(ratedNode);
-                                document.querySelector('.movie').appendChild(genreNode);
-                                document.querySelector('.movie').appendChild(plotNode);
-                              });
-                              aj.send();
-                            });  
-
-
-
-
-
+// start of movie details addition
+        node.addEventListener('click', function(){
+          document.querySelector(".movie").innerHTML = "";
+          var details = new XMLHttpRequest();
+          details.open('get', 'http://omdbapi.com/?t=' + this.innerHTML, true);
+          details.addEventListener('load', function(response){
+          var responses = ["Title", "Year", "Rated", "Genre", "Plot"];
+            for(var j = 0; j < responses.length; j++){
+              var newElement = document.createElement('li');
+              newElement.id = responses[j];
+              newElement.innerText = JSON.parse(this.response)[responses[j]];
+              document.querySelector('.movie').appendChild(newElement);
+            }
+          });
+          details.send();
+        });  
+// end of movie details
       }
   });
-    xhr.send();
+    movieSearch.send();
   });
 })();
-
+});
 
 
 
